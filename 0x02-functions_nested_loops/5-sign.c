6d5d47e54e04b2d6ae44105d310265178dfbd773
tarwk <tareqmomo214@gmail.com>
2023-10-10 06:59:09 -0700

    my commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,19 +10,21 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{_putchar(43);
+
+	{
+		_putchar(43);
 		return(1);
 	}
 	else if (n==0)
 	{ 
 		_putchar(48);
-		return(0) ;
+		return(0);
 
 	}
 	else 
 	{
-		_putchar(45) ;
-		return (-1) ;
+		_putchar(45);
+		return (-1);
 
 	}
 }
