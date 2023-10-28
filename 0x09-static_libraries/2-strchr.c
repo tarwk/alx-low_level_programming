c9d9d26214128424e748f084dfff9d96eb208de3
tarwk <tareqmomo214@gmail.com>
2023-10-28 14:43:46 -0700

    my commit

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,23 +1,18 @@
 #include "main.h"
-#include <stddef.h> 
 /**
-*_strchr - Returns a pointer to the first occurrence
-*of the character c in the string s, or NULL if the
-*character is not found
-*
-*@s:string targeted
-*@c:character targeted
-*
-*Return: returns pointer to first occcurence of c
-*/
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
-		return (s + i);
-	else
-		return (NULL);
+        	int i = 0;
+ 
+        	for (; s[i] >= '\0'; i++)
+        	{
+                    	if (s[i] == c)
+                                	return (&s[i]);
+        	}
+        	return (0);
 }
